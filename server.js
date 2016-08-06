89116510c63b30ecff18b99f5d0c04dff7b835fa
Philip Cooke <philipjcooke@gmail.com>
2016-08-06 09:50:15 -0500

    server js with ports

--- server.js
+++ server.js
@@ -3,6 +3,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override'); // for deletes in express
+var models = require("../models");
+var PORT = 3000;
+
 
 // MODEL CONTROLLERS IMPORT
 //==================================================
@@ -14,6 +17,10 @@ var mentors_controller = require('./controllers/mentors_controller.js');
 // EXPRESS SETUP
 //==================================================
 var app = express();
+app.set('port', process.env.PORT || 3000);
+app.listen(PORT, function(){
+	console.log('App listening on PORT ' + PORT);
+})
 
 //override POST to have DELETE and PUT
 app.use(methodOverride('_method'));
@@ -36,6 +43,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', home_controller);
 app.use('/mentees', mentees_controller);
-app.use('mentors', mentors_controller);
+app.use('/mentors', mentors_controller);
+
+// we bring in the models we exported with index.js
+
+
+//SYNC WITH SEQUELIZE
+models.sequelize.sync().then(function () {
+	// set our app to listen to the port we set above
+  var server = app.listen(app.get('port'), function() {
+  	// then save a log of the listening to our debugger.
+    debug('Express server listening on port ' + server.address().port);
+  });
+});
 
 module.exports = app;
