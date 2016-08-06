8159172b8831c5a120e97e8478c89a18b7c4f195
kittson <gazorna@gmail.com>
2016-08-06 10:57:24 -0500

    commiting server

--- server.js
+++ server.js
@@ -1,60 +1,26 @@
-// DEPENDENCIES
-//==================================================
 var express = require('express');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override'); // for deletes in express
-var models = require("../models");
-var PORT = 3000;
-
-
-// MODEL CONTROLLERS IMPORT
-//==================================================
-var home_controller = require('./controllers/home_controller.js');
-var mentees_controller = require('./controllers/mentees_controller');
-var mentors_controller = require('./controllers/mentors_controller.js');
-
-
-// EXPRESS SETUP
-//==================================================
 var app = express();
-app.set('port', process.env.PORT || 3000);
-app.listen(PORT, function(){
-	console.log('App listening on PORT ' + PORT);
-})
 
-//override POST to have DELETE and PUT
-app.use(methodOverride('_method'));
+var bodyParser = require('body-parser');
+var path = require('path');
 
-//view engine setup
-app.set('views', path.join(__dirname, 'views'));
+//app.use(express.static(path.join(__dirname, 'app/public')));
+app.use(express.static(path.normalize(path.join(__dirname, 'app/public'))));
+//var PORT = process.env.PORT || 8080;//must use this for heroku deployment
 
-// set up handlebars
-var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
-
-// bodyparsert
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-app.use('/', home_controller);
-app.use('/mentees', mentees_controller);
-app.use('/mentors', mentors_controller);
+//var homeController = require('./controllers/home_controller.js');
+//var menteesController = require('./controllers/mentees_controller.js');
+var mentorsController = require('./controllers/mentors_controller.js');
 
-// we bring in the models we exported with index.js
+//app.use('/', homeController);
+//app.use('/', menteesController);
+app.use('/', mentorsController);
 
-
-//SYNC WITH SEQUELIZE
-models.sequelize.sync().then(function () {
-	// set our app to listen to the port we set above
-  var server = app.listen(app.get('port'), function() {
-  	// then save a log of the listening to our debugger.
-    debug('Express server listening on port ' + server.address().port);
-  });
-});
-
-module.exports = app;
+app.listen(3000, function() {
+  console.log('Listening on port ') // + PORT)
+})
