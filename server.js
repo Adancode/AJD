1fba784df7cc3d521f25e51878f0b5988fc98d4f
Philip Cooke <philipjcooke@gmail.com>
2016-08-06 09:27:23 -0500

    adding server.js code

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
+// DEPENDENCIES
 var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override'); // for deletes in express
+
+// MODEL CONTROLLERS IMPORT
